96927c3825aff9b1a1acb3944a27a541f408be66
Matteo Brambilla <bramba2000@gmail.com>
2025-02-25 17:14:26 +0100

    add opa engine resource CRD and stub controller

---
+++ api/v1alpha1/opaengine_types.go
@@ -0,0 +1,75 @@
+/*
+Copyright 2025 Matteo Brambilla <matteo15.brambilla@polimi.it>.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// OpaEngineSpec defines the desired state of OpaEngine
+type OpaEngineSpec struct {
+	// Image to use for the OPA engine
+	// +kubebuilder:default:value="openpolicyagent/opa:latest-envoy"
+	Image string `json:"image,omitempty"`
+
+	// Number of replicas for the OPA engine
+	// +kubebuilder:default:value=1
+	// +kubebuilder:validation:Minimum=1
+	Replicas int32 `json:"replicas,omitempty"`
+
+	// +kubebuilder:optional
+	Resources corev1.ResourceRequirements `json:"resources"`
+}
+
+// OpaEngineStatus defines the observed state of OpaEngine
+type OpaEngineStatus struct {
+	// Represent the observations of a OpaEngine's current state
+	// OpaEngine.status.conditions.type are : "Available", "Progressing", "Degraded"
+	// OpaEngine.status.conditions.status are : "True", "False", "Unknown"
+
+	// +operator-sdk:csv:customresourcedefinitions:type=status
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,1,rep,name=conditions"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// OpaEngine is the Schema for the opaengines API
+type OpaEngine struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   OpaEngineSpec   `json:"spec,omitempty"`
+	Status OpaEngineStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// OpaEngineList contains a list of OpaEngine
+type OpaEngineList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []OpaEngine `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&OpaEngine{}, &OpaEngineList{})
+}
---
+++ internal/controller/opaengine_controller_test.go
@@ -0,0 +1,92 @@
+/*
+Copyright 2025 Matteo Brambilla <matteo15.brambilla@polimi.it>.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	opaspolimiitv1alpha1 "github.com/bramba2000/opa-scaler/api/v1alpha1"
+)
+
+var _ = Describe("OpaEngine Controller", func() {
+	Context("When reconciling a resource", func() {
+		const resourceName = "test-resource"
+
+		ctx := context.Background()
+
+		typeNamespacedName := types.NamespacedName{
+			Name:      resourceName,
+			Namespace: "default", // TODO(user):Modify as needed
+		}
+		opaengine := &opaspolimiitv1alpha1.OpaEngine{}
+
+		BeforeEach(func() {
+			By("creating the custom resource for the Kind OpaEngine")
+			err := k8sClient.Get(ctx, typeNamespacedName, opaengine)
+			if err != nil && errors.IsNotFound(err) {
+				resource := &opaspolimiitv1alpha1.OpaEngine{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      resourceName,
+						Namespace: "default",
+					},
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+		})
+
+		AfterEach(func() {
+			// TODO(user): Cleanup logic after each test, like removing the resource instance.
+			resource := &opaspolimiitv1alpha1.OpaEngine{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Cleanup the specific resource instance OpaEngine")
+			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+		})
+		It("should successfully set defaults", func() {
+			resource := &opaspolimiitv1alpha1.OpaEngine{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(resource.Spec.Replicas).To(Equal(int32(1)))
+			Expect(resource.Spec.Image).To(Equal("openpolicyagent/opa:latest-envoy"))
+		})
+
+		It("should successfully reconcile the resource", func() {
+			By("Reconciling the created resource")
+			controllerReconciler := &OpaEngineReconciler{
+				Client: k8sClient,
+				Scheme: k8sClient.Scheme(),
+			}
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
+			// Example: If you expect a certain status condition after reconciliation, verify it here.
+		})
+	})
+})
